7294977e9cf4d190d92f73fed39abbecc60dfc7e
Luana Duarte <luludsf@gmail.com>
2019-10-16 09:22:37 -0300

    changed the html format of variables to markdown

--- handlers/commands_handler.py
+++ handlers/commands_handler.py
@@ -35,29 +35,29 @@ group_list():
 """
 def group_list(update, context):
 
-    text_grupos_estudos = ("Lista de grupos de estudos:\n"
-    '<a href="https://t.me/joinchat/CIoz-gwgPwKC8g8i5-CzZg"> Álgebra Linear (AL) </a>\n'
-	'<a href="https://t.me/joinchat/IqDW5RFkiJ9MMVD11AJ_-Q"> Cálculo III </a>\n'
-    '<a href="https://t.me/joinchat/CIoz-hRPWS_bAOXEptVP5Q"> Computação Gráfica (CG) </a>\n'
-	'<a href="https://t.me/joinchat/IEr6PE6mw8p-k9C1wzUdvQ"> Redes de Computadores II (REDES-II) </a>\n'
-	'<a href="https://t.me/joinchat/CIoz-lfkQXIwBPcwoee3-g"> Processamento de Imagens Digitais (PID) </a>\n'
-	'<a href="https://t.me/joinchat/CIoz-hZQxMd1JzjfZjRRLg"> Tópicos I/III </a>\n'
-	'<a href="https://t.me/joinchat/HQIiXRITL-Pg2UZaYMMQ3A"> Tópicos II/IV </a>\n'
-	'<a href="https://t.me/joinchat/HQIiXRFuRCYdoIetfodKSw"> Tópicos Virtual </a>\n'
-	'<a href="https://t.me/joinchat/IEr6PEUFqPwQB3b3SeoLzQ"> TCC 1 </a>\n'
-	'<a href="https://t.me/joinchat/IEr6PFScE-ROAHwJK0FcsA"> Compiladores </a>\n'
-	'<a href="https://t.me/joinchat/IEr6PFcFdfzlT_sw0s8nEw"> Inteligência Artificial (IA) </a>\n'
-	'<a href="https://t.me/joinchat/IEr6PEr1znSyU7dokXigGw"> Otimização de Sistemas (OS) </a>\n'
-	'<a href="https://t.me/joinchat/IEr6PFQ43feuPMbd_Mzkhg"> Lab. de Redes e S.O </a>\n'
-                          )
+        text_grupos_estudos = ("Lista de grupos de estudos:\n"
+        "[Álgebra Linear (AL)](https://t.me/joinchat/CIoz-gwgPwKC8g8i5-CzZg)\n"
+        "[Cálculo III](https://t.me/joinchat/IqDW5RFkiJ9MMVD11AJ_-Q)\n"
+        "[Computação Gráfica (CG)](https://t.me/joinchat/CIoz-hRPWS_bAOXEptVP5Q)\n"
+        "[Redes de Computadores II (REDES-II)](https://t.me/joinchat/IEr6PE6mw8p-k9C1wzUdvQ)\n"
+        "[Processamento de Imagens Digitais (PID)](https://t.me/joinchat/CIoz-lfkQXIwBPcwoee3-g)\n"
+        "[Tópicos I/III](https://t.me/joinchat/CIoz-hZQxMd1JzjfZjRRLg)\n"
+        "[Tópicos II/IV](https://t.me/joinchat/HQIiXRITL-Pg2UZaYMMQ3A)\n"
+        "[Tópicos Virtual](https://t.me/joinchat/HQIiXRFuRCYdoIetfodKSw)\n"
+        "[TCC 1](https://t.me/joinchat/IEr6PEUFqPwQB3b3SeoLzQ)\n"
+        "[Compiladores](https://t.me/joinchat/IEr6PFScE-ROAHwJK0FcsA)\n"
+        "[Inteligência Artificial (IA)](https://t.me/joinchat/IEr6PFcFdfzlT_sw0s8nEw)\n"
+        "[Otimização de Sistemas (OS)](https://t.me/joinchat/IEr6PEr1znSyU7dokXigGw)\n"
+        "[Lab. de Redes e S.O](https://t.me/joinchat/IEr6PFQ43feuPMbd_Mzkhg)\n"
+    )
     text_grupos_geral = ("Lista de outros grupos: \n"
-        '<a href="https://t.me/joinchat/IEr6PEhoU9TdPmkaJli3GA"> Grupo de estudos </a>\n'
-        '<a href="https://t.me/joinchat/IEr6PESXH6aDuWon4j3CoA"> Jogo do Lobinho </a> Venha se matar com a gente !\n'
-        )
+        "[Grupo de estudos](https://t.me/joinchat/IEr6PEhoU9TdPmkaJli3GA) \n" 
+        "[Jogo do Lobinho](https://t.me/joinchat/IEr6PESXH6aDuWon4j3CoA) Venha se matar com a gente!\n"
+    )                      
         
     context.bot.send_message(chat_id = update.message.chat_id,
         text = text_grupos_estudos + text_grupos_geral,
-        parse_mode = telegram.ParseMode.HTML,
+        parse_mode = telegram.ParseMode.MARKDOWN,
         disable_web_page_preview=True)
 pass
 
