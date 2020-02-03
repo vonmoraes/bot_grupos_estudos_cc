ac0677f02c9643c27bb5041ddc931a646af2f19e
Lucas de Souza Moraes <moraessouzalucas@gmail.com>
2020-02-03 16:42:22 -0300

    Update commands_handler.py

--- handlers/commands_handler.py
+++ handlers/commands_handler.py
@@ -42,6 +42,7 @@ def group_list(update, context):
 	    '<a href="https://t.me/joinchat/DbdJcBRZvaq3xXs1mFn9ig"> Release Engineering  </a>\n'
 	    '<a href="https://t.me/joinchat/DbdJcBN6Dl_cYqVWSNBqSA"> Segurança e Auditoria de Sistemas </a>\n'
 	    '<a href="https://t.me/joinchat/DbdJcBAZKYbHscWnIw4JaA"> Computaçao Distribuída </a>\n'
+	    '<a href="https://t.me/joinchat/DbdJcBS0uSo6C-VQdpeD8w"> Topicos II </a>\n'
 	    '<a href="https://t.me/joinchat/D0xe4lPO_i3KJN4hIdlFZg"> TCC 1 </a>\n'
 	    '<a href="https://t.me/joinchat/HQIiXUUFqPwTLlWoc-V2ug"> TCC 2 </a>\n'
     )
